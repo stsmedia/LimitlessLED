9a8ae6edba1c6583f27fe8fd9d5643e0b0e334f6
Stefan Schmidt <stsmedia@gmail.com>
2013-10-13 14:58:37 +1100

    Keep console open for executing more commands

--- LimitlessLED-Test/Program.cs
+++ LimitlessLED-Test/Program.cs
@@ -4,61 +4,77 @@ namespace LimitlessLED_Test
 {
     class Program
     {
+        private static bool runProgram = true;
         /// <summary>
         /// This is control code for the LimitlessLED light bulbs.  It's currently pretty much under first draft testing.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            // Process Command Line Arguments
-            if (args.Length != 0)
+            var program = new Program();
+            while (runProgram)
             {
-                switch (args[0])
+                var command = Console.ReadLine();
+                // Process Command Line Arguments
+                if (command.Length != 0)
                 {
-                    case       "allOff":
-                    case       "alloff": { UserCommands.AllOff(); return; }
-
-                    case        "allOn":
-                    case        "allon": { UserCommands.AllOn(); return; }
-
-                    case     "brighten": { UserCommands.Brighten(); return; }
-
-                    case          "dim": { UserCommands.Dim(); return; }
-
-                    case "allNightMode":
-                    case "allnightmode": { UserCommands.AllNightMode(); return; }
-
-                    case    "group1off": { UserCommands.Group1Off(); return; }
-                    case    "group2off": { UserCommands.Group2Off(); return; }
-                    case    "group3off": { UserCommands.Group3Off(); return; }
-                    case    "group4off": { UserCommands.Group4Off(); return; }
-
-                    case  "group1night": { UserCommands.Group1NightMode(); return; }
-                    case  "group2night": { UserCommands.Group2NightMode(); return; }
-                    case  "group3night": { UserCommands.Group3NightMode(); return; }
-                    case  "group4night": { UserCommands.Group4NightMode(); return; }
-
-                    case         "test": { UserCommands.Test(); return; } // for testing experimental code
-
-                    case        "flash":
-                    case       "strobe":
-                    case        "blink": { UserCommands.StrobeMode(); return; }
-
-                    case       "fadeUp":
-                    case       "fadeup": { UserCommands.FadeUp(); return; }
-
-                    case     "fadeDown":
-                    case     "fadedown": { UserCommands.FadeDown(); return; }
-
-                    case   "wakeupcall": { UserCommands.WakeUpCall(); return; }
-
-                    case        "rgbon": { UserCommands.RGBOn(); return; }
-                    case       "rgboff": { UserCommands.RGBOff(); return; }
-
-                    default: { Console.WriteLine("I have no idea what you wanted me to do"); return; } // Tell the user we have no idea WTF they wanted
+                    program.RunCommand(command);
                 }
-            }         
-            Console.WriteLine("There are input arguments required.  I really should put something here to explain them for you");
+                else
+                {
+                    Console.WriteLine("There are input arguments required.  I really should put something here to explain them for you");
+                }
+            }
+        }
+
+        private void RunCommand(string command)
+        {
+            switch (command)
+            {
+                case "allOff":
+                case "alloff": { UserCommands.AllOff(); return; }
+
+                case "allOn":
+                case "allon": { UserCommands.AllOn(); return; }
+
+                case "brighten": { UserCommands.Brighten(); return; }
+
+                case "dim": { UserCommands.Dim(); return; }
+
+                case "allNightMode":
+                case "allnightmode": { UserCommands.AllNightMode(); return; }
+
+                case "group1off": { UserCommands.Group1Off(); return; }
+                case "group2off": { UserCommands.Group2Off(); return; }
+                case "group3off": { UserCommands.Group3Off(); return; }
+                case "group4off": { UserCommands.Group4Off(); return; }
+
+                case "group1night": { UserCommands.Group1NightMode(); return; }
+                case "group2night": { UserCommands.Group2NightMode(); return; }
+                case "group3night": { UserCommands.Group3NightMode(); return; }
+                case "group4night": { UserCommands.Group4NightMode(); return; }
+
+                case "test": { UserCommands.Test(); return; } // for testing experimental code
+
+                case "flash":
+                case "strobe":
+                case "blink": { UserCommands.StrobeMode(); return; }
+
+                case "fadeUp":
+                case "fadeup": { UserCommands.FadeUp(); return; }
+
+                case "fadeDown":
+                case "fadedown": { UserCommands.FadeDown(); return; }
+
+                case "wakeupcall": { UserCommands.WakeUpCall(); return; }
+
+                case "rgbon": { UserCommands.RGBOn(); return; }
+                case "rgboff": { UserCommands.RGBOff(); return; }
+
+                case "quit": { runProgram = false; return; }
+
+                default: { Console.WriteLine("I have no idea what you wanted me to do"); return; } // Tell the user we have no idea WTF they wanted
+            }
         }
     }
 }
